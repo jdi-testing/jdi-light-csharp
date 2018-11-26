95f9453c8ad40103bcb9ab20b45ee5d7a8773a4f
Evgeniy Kosjakov <42_@mail.ru>
2018-11-26 19:30:53 +0300

    new project for tests

---
+++ JDI.Light/JDI.Light.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JDI.Light.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("JDI.Light.Tests")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("73e43896-6a55-45b2-b831-0d9e48ed4031")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
