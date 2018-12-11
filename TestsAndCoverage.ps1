c4f2c5f1d816d506f9003848375018101547c961
Evgeniy Kosjakov <42_@mail.ru>
2018-12-11 11:26:50 +0300

    write-host data updated

--- TestsAndCoverage.ps1
+++ TestsAndCoverage.ps1
@@ -2,14 +2,14 @@
 
 $coveralls = (Resolve-Path "JDI.Light/packages/coveralls.net.*/tools/csmacnz.coveralls.exe").ToString()
 
-write-host $coveralls
-write-host $env:APPVEYOR_PULL_REQUEST_NUMBER
+write-host "======= COVERALLS PATH: " $coveralls " ======="
 
 if ($env:APPVEYOR_PULL_REQUEST_NUMBER -eq $null) {
 	& $coveralls --opencover -i opencoverCoverage.xml --repoToken $env:COVERALLS_REPO_TOKEN --commitId $env:APPVEYOR_REPO_COMMIT --commitBranch $env:APPVEYOR_REPO_BRANCH --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE --jobId $env:APPVEYOR_JOB_ID
 }
 else {
-	& $coveralls --opencover -i opencoverCoverage.xml --repoToken $env:COVERALLS_REPO_TOKEN #--jobId $env:APPVEYOR_JOB_ID --pullRequest $env:APPVEYOR_PULL_REQUEST_NUMBER
+	write-host "======= PULL REQUEST: " $env:APPVEYOR_PULL_REQUEST_NUMBER " ======="
+	& $coveralls --opencover -i opencoverCoverage.xml --repoToken $env:COVERALLS_REPO_TOKEN
 }
 
 $result = $LASTEXITCODE
