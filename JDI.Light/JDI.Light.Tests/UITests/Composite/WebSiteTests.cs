478bc039b7169864e8fcabd7fc97f83baa8ef0d6
Evgeniy Kosjakov <42_@mail.ru>
2019-02-05 19:27:04 +0300

    web site tests added

--- JDI.Light/JDI.Light.Tests/UITests/Composite/WebSiteTests.cs
+++ JDI.Light/JDI.Light.Tests/UITests/Composite/WebSiteTests.cs
@@ -20,5 +20,48 @@ namespace JDI.Light.Tests.UITests.Composite
             TestSite.OpenUrl(url);
             Assert.AreEqual(url, TestSite.Url);
         }
+
+        [Test]
+        public void OpenBaseUrlTest()
+        {
+            TestSite.OpenBaseUrl();
+            Assert.AreEqual("https://epam.github.io/", TestSite.Url);
+        }
+        
+        [Test]
+        public void RefreshTest()
+        {
+            TestSite.ContactFormPage.Open();
+            TestSite.ContactFormPage.CheckOpened();
+            TestSite.ContactFormPage.ContactSubmit.Click();
+            Jdi.Assert.Contains(TestSite.ContactFormPage.Result.Value, "Summary: 3");
+            TestSite.Refresh();
+            Jdi.Assert.AreEquals(TestSite.ContactFormPage.Result.Value, "");
+            TestSite.ContactFormPage.CheckOpened();
+        }
+
+        [Test]
+        public void BackTest()
+        {
+            TestSite.ContactFormPage.Open();
+            TestSite.ContactFormPage.CheckOpened();
+            TestSite.HomePage.Open();
+            TestSite.HomePage.CheckOpened();
+            TestSite.Back();
+            TestSite.ContactFormPage.CheckOpened();
+        }
+
+        [Test]
+        public void ForwardTest()
+        {
+            TestSite.ContactFormPage.Open();
+            TestSite.ContactFormPage.CheckOpened();
+            TestSite.HomePage.Open();
+            TestSite.HomePage.CheckOpened();
+            TestSite.Back();
+            TestSite.ContactFormPage.CheckOpened();
+            TestSite.Forward();
+            TestSite.HomePage.CheckOpened();
+        }
     }
 }
