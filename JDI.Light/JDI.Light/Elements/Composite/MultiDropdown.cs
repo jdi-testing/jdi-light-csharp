d652f4d32580e14092a21627b5bee7c296c9ae4f
Alexander Seregin <service@mongo-software.ru>
2019-03-11 12:24:44 +0300

    Initial commit

---
+++ JDI.Light/JDI.Light/Elements/Composite/MultiDropdown.cs
@@ -0,0 +1,19 @@
+﻿using JDI.Light.Elements.Base;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JDI.Light.Elements.Composite
+{
+    public class MultiDropdown : UIElement
+    {
+        public MultiDropdown(By locator): base (locator)
+        {
+
+        }
+
+    }
+}
