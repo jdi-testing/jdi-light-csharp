117527d67fb7b6b969eac14c2bc8a1c52bba4593
AnnaDreval <annadreval@yandex.ru>
2019-03-21 15:07:35 +0300

    Implemented ComboBoxes

---
+++ JDI.Light/JDI.Light.Tests/Tests/Common/ComboBoxTests.cs
@@ -0,0 +1,48 @@
+﻿using JDI.Light.Tests.Enums;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace JDI.Light.Tests.Tests.Common
+{
+    [TestFixture]
+    public class ComboBoxTests : TestBase
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            TestSite.MetalsColorsPage.Open();
+            TestSite.MetalsColorsPage.CheckOpened();
+        }
+
+        [Test]
+        public void SelectComboBox()
+        {
+            TestSite.MetalsColorsPage.OpenComboBox();
+            TestSite.MetalsColorsPage.MetalsComboBox.ItemLocator = By.XPath("//li/a");
+            TestSite.MetalsColorsPage.MetalsComboBox.Select(Metals.Bronze.ToString());
+
+            Jdi.Assert.Contains(TestSite.ActionsLog.Texts[0], "Metals: value changed to Bronze");
+        }
+
+        [Test]
+        public void SelectByIndex()
+        {
+            const int indexToSelect = 2;
+
+            TestSite.MetalsColorsPage.OpenComboBox();
+            TestSite.MetalsColorsPage.MetalsComboBox.ItemLocator = By.CssSelector(string.Format($"li:nth-child({indexToSelect.ToString()})"));
+            TestSite.MetalsColorsPage.MetalsComboBox.Select(indexToSelect);
+
+            Jdi.Assert.Contains(TestSite.ActionsLog.Texts[0], "Metals: value changed to Gold");
+        }
+
+        [Test]
+        public void FillComboBox()
+        {
+            TestSite.MetalsColorsPage.MetalsInputComboBox.Input("My Metal");
+            TestSite.MetalsColorsPage.SubmitButton.Click();
+
+            Jdi.Assert.Contains(TestSite.ActionsLog.Texts[1], "Metals: value changed to My Metal");
+        }
+    }
+}
--- JDI.Light/JDI.Light.Tests/UIObjects/Pages/MetalsColorsPage.cs
+++ JDI.Light/JDI.Light.Tests/UIObjects/Pages/MetalsColorsPage.cs
@@ -56,9 +56,23 @@ namespace JDI.Light.Tests.UIObjects.Pages
         [FindBy(Css = "#submit-button")]
         public Button SubmitButton { get; set; }
 
+        [FindBy(Css = "#metals")]
+        public ComboBox MetalsComboBox { get; set; }
+
+        [FindBy(Css = "#metals span.caret")]
+        public ComboBox MetalsComboBoxCaret { get; set; }
+
+        [FindBy(Css = "#metals input")]
+        public ComboBox MetalsInputComboBox { get; set; }
+
         public void OpenDataList()
         {
             MetalsDataListCaret.Click();
+        }
+
+        public void OpenComboBox()
+        {
+            MetalsComboBoxCaret.Click();
         }
     }
 }
