e97f3d671b537d8fa49fc29d3a4ad1a2d268b729
dmitriev dmitrii <Dmitrii_Dmitriev@epam.com>
2019-04-17 15:47:14 +0300

    added locators to ICheckList

--- JDI.Light/JDI.Light/Interfaces/Complex/ICheckList.cs
+++ JDI.Light/JDI.Light/Interfaces/Complex/ICheckList.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Generic;
 using JDI.Light.Interfaces.Base;
+using OpenQA.Selenium;
 
 namespace JDI.Light.Interfaces.Complex
 {
     public interface ICheckList : IBaseUIElement, IGetValue<List<string>>
     {
+        By CheckListLocator { get; set; }
+        By LabelLocator { get; set; }
         void Check(params string[] values);
         void Check(params int[] indexes);
         void Uncheck(params string[] values);
