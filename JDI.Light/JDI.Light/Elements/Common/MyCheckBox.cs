b5baebeb22dcadc663f933fd9f08604f4a6e43f2
dmitriev dmitrii <Dmitrii_Dmitriev@epam.com>
2019-04-19 15:44:13 +0300

    test class

---
+++ JDI.Light/JDI.Light/Elements/Common/MyCheckBox.cs
@@ -0,0 +1,31 @@
+﻿using JDI.Light.Elements.Base;
+using JDI.Light.Interfaces.Base;
+using JDI.Light.Interfaces.Common;
+using OpenQA.Selenium;
+
+namespace JDI.Light.Elements.Common
+{
+    public class MyCheckBox : UIElement, ICheckBox
+    {
+        protected MyCheckBox(By byLocator) : base(byLocator)
+        {
+        }
+
+        bool IGetValue<bool>.Value => true;
+
+        bool ISetValue<bool>.Value { get; set; }
+ 
+
+        public void Check()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Uncheck()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsChecked => false;
+    }
+}
