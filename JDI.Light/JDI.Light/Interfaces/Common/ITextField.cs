477f70ebee9f8aa7b09c929b56b5cf330898000e
dmitriev dmitrii <Dmitrii_Dmitriev@epam.com>
2019-04-25 17:35:45 +0300

    removed extra methods declaration

--- JDI.Light/JDI.Light/Interfaces/Common/ITextField.cs
+++ JDI.Light/JDI.Light/Interfaces/Common/ITextField.cs
@@ -1,14 +1,11 @@
-﻿using JDI.Light.Interfaces.Asserts;
-using JDI.Light.Interfaces.Base;
+﻿using JDI.Light.Interfaces.Base;
 
 namespace JDI.Light.Interfaces.Common
 {
-    public interface ITextField : ISetValue<string>, ITextElement, IHasIsAssert
+    public interface ITextField : ISetValue<string>, ITextElement
     {
         void Input(string text);
-        new void SendKeys(string text);
         void SetText(string text);
-        new void Clear();
         void Focus();
         string Placeholder { get; }
     }
