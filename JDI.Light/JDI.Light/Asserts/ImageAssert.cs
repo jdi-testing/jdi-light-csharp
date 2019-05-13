cfc520c9d47e3806991867446cc3220b940b2222
AnnaDreval <annadreval@yandex.ru>
2019-05-13 13:40:37 +0300

    Image asserts implemented

---
+++ JDI.Light/JDI.Light/Asserts/ImageAssert.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using JDI.Light.Asserts.Generic;
+using JDI.Light.Elements.Common;
+using JDI.Light.Matchers;
+using static JDI.Light.Jdi;
+
+namespace JDI.Light.Asserts
+{
+    public class ImageAssert : IsAssert<ImageAssert>
+    {
+        protected Image Image { get; }
+
+        public ImageAssert(Image image) : base(image)
+        {
+            Image = image;
+        }
+
+        public ImageAssert Src(Matcher<string> condition)
+        {
+            Assert.IsTrue(condition.IsMatch(Image.Src), $"The src value {Image.Src} is not {condition.ActionName} {condition.RightValue}");
+            return this;
+        }
+
+        public ImageAssert Alt(Matcher<string> condition)
+        {
+            Assert.IsTrue(condition.IsMatch(Image.Alt), $"The alt value {Image.Alt} is not {condition.ActionName} {condition.RightValue}");
+            return this;
+        }
+
+        public ImageAssert Height(Matcher<int> condition)
+        {
+            Assert.IsTrue(condition.IsMatch(Convert.ToInt32(Image.Height)), $"The height value {Image.Height} is not {condition.ActionName} {condition.RightValue}");
+            return this;
+        }
+
+        public ImageAssert Width(Matcher<int> condition)
+        {
+            Assert.IsTrue(condition.IsMatch(Convert.ToInt32(Image.Width)), $"The width value {Image.Width} is not {condition.ActionName} {condition.RightValue}");
+            return this;
+        }
+    }
+}
