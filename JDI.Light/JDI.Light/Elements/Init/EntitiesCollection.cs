bd5b5d88723b971ed613332dca7e149ece620936
Anna Belemenko <Anna_Belemenko@epam.com>
2020-05-12 14:05:35 +0300

    Implemented filling of Pages and Elements collections for BDD-steps.

---
+++ JDI.Light/JDI.Light/Elements/Init/EntitiesCollection.cs
@@ -0,0 +1,12 @@
+﻿using JDI.Light.Interfaces.Base;
+using JDI.Light.Interfaces.Composite;
+using System.Collections.Generic;
+
+namespace JDI.Light.Elements.Init
+{
+    public class EntitiesCollection
+    {
+        public static Dictionary<string, IPage> Pages = new Dictionary<string, IPage>();
+        public static Dictionary<string, List<IBaseElement>> Elements = new Dictionary<string, List<IBaseElement>>();
+    }
+}
