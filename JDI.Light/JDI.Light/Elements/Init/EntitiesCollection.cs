f67274cfcf0b8c2c120850648d6da2b8c4818d01
Vladimir Vdovichenko <Vladimir_Vdovichenko@epam.com>
2020-05-13 15:29:11 +0300

    getPage generic with Type cast

--- JDI.Light/JDI.Light/Elements/Init/EntitiesCollection.cs
+++ JDI.Light/JDI.Light/Elements/Init/EntitiesCollection.cs
@@ -1,5 +1,6 @@
 ﻿using JDI.Light.Interfaces.Base;
 using JDI.Light.Interfaces.Composite;
+using System;
 using System.Collections.Generic;
 
 namespace JDI.Light.Elements.Init
@@ -11,7 +12,31 @@ namespace JDI.Light.Elements.Init
         }
 
         public static Dictionary<string, IPage> Pages { get; set; } = new Dictionary<string, IPage>();
-
         public static Dictionary<string, List<IBaseElement>> Elements { get; set; } = new Dictionary<string, List<IBaseElement>>();
+
+        public static T getPage<T>(string pageName, T type)
+        {
+            var page = getPage(pageName);
+            if (page != null && page.GetType().IsClass)
+                return (T)page;
+            throw new Exception($"Can't cast element {page.GetType()} to {type}");
+        }
+
+        public static IPage getPage(string pageName)
+        {
+            if (Pages.ContainsKey(pageName))
+            {
+                return Pages[pageName];
+            }
+            else
+            if (Pages.ContainsKey(pageName + " Page"))
+            {
+                return Pages[pageName + " Page"];
+            }
+            else
+            {
+                throw new Exception($"No page found with name {pageName}");
+            }
+        }
     }
 }
