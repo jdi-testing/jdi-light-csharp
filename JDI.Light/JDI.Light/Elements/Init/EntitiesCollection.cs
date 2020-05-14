eb1600a98db0bcfa9992c123ea206ff5288dcdad
Vladimir Vdovichenko <Vladimir_Vdovichenko@epam.com>
2020-05-14 15:13:50 +0300

    Fix codacy issues

--- JDI.Light/JDI.Light/Elements/Init/EntitiesCollection.cs
+++ JDI.Light/JDI.Light/Elements/Init/EntitiesCollection.cs
@@ -1,4 +1,5 @@
-﻿using JDI.Light.Interfaces.Base;
+﻿using JDI.Light.Exceptions;
+using JDI.Light.Interfaces.Base;
 using JDI.Light.Interfaces.Composite;
 using System;
 using System.Collections.Generic;
@@ -17,9 +18,18 @@ namespace JDI.Light.Elements.Init
         public static T getPage<T>(string pageName, T type)
         {
             var page = getPage(pageName);
-            if (page != null && page.GetType().IsClass)
-                return (T)page;
-            throw new Exception($"Can't cast element {page.GetType()} to {type}");
+            if (page != null)
+            {
+                if (page.GetType().IsClass)
+                {
+                    return (T)page;
+                }
+                else
+                {
+                    throw new InvalidCastException($"Can't cast element {page.GetType()} to {type}");
+                }
+            }
+            throw new ElementNotFoundException($"No page found with name {pageName}");
         }
 
         public static IPage getPage(string pageName)
@@ -35,7 +45,7 @@ namespace JDI.Light.Elements.Init
             }
             else
             {
-                throw new Exception($"No page found with name {pageName}");
+                throw new ElementNotFoundException($"No page found with name {pageName}");
             }
         }
     }
