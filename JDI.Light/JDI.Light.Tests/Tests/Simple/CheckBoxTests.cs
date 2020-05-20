4df33508795aeb7b9ad2066a2a442d18a27a8c00
tsvetaev.alex <tsvetaev.alex@gmail.com>
2020-05-20 16:00:55 +0300

    Update CheckBoxTests.cs
    CheckBoxTests.cs remove QA clarification comment

--- JDI.Light/JDI.Light.Tests/Tests/Simple/CheckBoxTests.cs
+++ JDI.Light/JDI.Light.Tests/Tests/Simple/CheckBoxTests.cs
@@ -23,11 +23,6 @@ namespace JDI.Light.Tests.Tests.Simple
         [Test]
         public void CheckSingleTest()
         {
-            /*
-            5/19/2020: update Alex T in line 30: remove true
-            Reason: true used as default value
-            */ 
-            CbWater.Check();
             Assert.DoesNotThrow(() => TestSite.MetalsColorsPage.CbWater.Check());
             Jdi.Assert.Contains(TestSite.ActionsLog.Texts[0], "Water: condition changed to true");
         }
