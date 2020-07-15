6fe07bdb59abebd872ca81d70e9cc83bc88f73ba
tsvetaevAlex <56589282+tsvetaevAlex@users.noreply.github.com>
2020-07-15 19:01:05 +0300

    modify client class login password fields

--- JDI.Light/JDI.Light.Tests/Entities/Client.cs
+++ JDI.Light/JDI.Light.Tests/Entities/Client.cs
@@ -4,8 +4,13 @@ namespace JDI.Light.Tests.Entities
 {
     public class Client
     {
-        public static Client DefaultClient = new Client("epam", "1234");
-        public string Login, Password;
+        public readonly Client DefaultClient;
+        protected string Login, Password;
+
+        public Client()
+        {
+            DefaultClient = new Client("epam", "1234");
+        }
 
         public Client(string login, string password)
         {
